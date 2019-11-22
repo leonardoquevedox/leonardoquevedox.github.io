3c71966e37569019ff68b9e63ca789e1f45d190b
Leonardo Quevedo <pac.leo@hotmail.com>
2019-11-22 01:36:01 -0300

    Adds fixes to the landing page content.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -32,8 +32,28 @@ const Layout = ({ children, location }) => {
       render={data => (
         <>
           <Helmet title={data.site.siteMetadata.title}>
-            <html lang="en" />
-            
+            <html lang="pt-BR" />
+            {/* <title>
+              Psicóloga em São Leopoldo, Porto Alegre e Online | Psicóloga Priscila de Melo | Terapia
+            </title>
+            <meta
+              name="description"
+              content="Psicóloga clínica e social. Mestrado em Saúde Coletiva. Atendimentos individuais com crianças, adolescentes e adultos. Terapia de casal. Atendimento clínico."
+            />
+            <meta
+              name="keywords"
+              content="Psicologia, Psicóloga, Terapia, Psicoterapia, Atendimento psicológico, Avaliação psicológica, Psicóloga infantil, Psicóloga criança, Psicóloga casal, Psicóloga São Leopoldo, Psicóloga Porto Alegre, Psicóloga Online"
+            />
+            <meta name="robots" content="" />
+            <meta name="revisit-after" content="1 day" />
+            <meta name="language" content="Portuguese" />
+
+            <meta charset="UTF-8" />
+            <meta
+              name="twitter:title"
+              content="Psicóloga Priscila de Melo | Psicóloga em São Leopoldo, Porto Alegre e Online"
+            />
+            <link rel="shortcut icon" href="assets/images/favicon-2.png" type="image/x-icon" /> */}
           </Helmet>
           {content}
         </>
