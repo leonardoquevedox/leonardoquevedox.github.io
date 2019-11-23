34ceb6fbf5a6376e6dc8b69f8c5ac57457525380
Leonardo Quevedo <pac.leo@hotmail.com>
2019-11-23 17:55:26 -0300

    Updates Github page version.

--- scripts/deploy/index.js
+++ scripts/deploy/index.js
@@ -9,13 +9,13 @@ const { exec } = shell
 
 const run = async () => {
   console.log(`☮   Github page: Generating commit...`.yellow)
-
   const commitMsg = 'Updates Github page version.'
+  await exec(`cp ./CNAME ./public/CNAME`, { cwd: rootPath })
   await exec('git add .', { cwd: rootPath })
   await exec(`git commit -m '${commitMsg}'`, { cwd: rootPath })
-  await exec(`cp ./CNAME ./public/CNAME`, { cwd: rootPath })
-  await exec(`git subtree push --prefix public origin gh-pages --force`, { cwd: rootPath })
+  await exec(`git subtree split --prefix public -b gh-pages`, { cwd: rootPath })
   await exec(`git push origin`, { cwd: rootPath })
+  await exec(`git branch -D gh-pages`, { cwd: rootPath })
   process.exit()
 }
 
