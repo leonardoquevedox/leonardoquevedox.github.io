86a94707e3864fd3b56f46377e5c9a72a93ff90a
Leonardo Quevedo <leonardoq@trin.ca>
2021-09-14 13:34:37 -0300

    🔥 (v2) Major structure refactor: Initial changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,88 @@
-import HomePage from './home'
+import React, { useCallback, useState, useEffect } from 'react'
 
-// Just a better
+import PropTypes from 'prop-types'
+
+import Layout from 'components/Layout'
+import Header from 'components/Header'
+import Main from 'components/Main/Main'
+import Footer from 'components/Footer'
+
+let openingTimeoutRef = null
+
+const HomePage = ({ location }) => {
+  const [isSectionVisible, setIsSectionVisible] = useState(false)
+  const [hasTimeout, setHasTimeout] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
+  const [article, setSection] = useState('')
+  const [loading, setLoading] = useState('is-loading')
+  const [wrapperRef, setWrapperRef] = useState(null)
+
+  const toggleSection = useCallback(() => {
+    setIsSectionVisible(!isSectionVisible)
+    setHasTimeout(!hasTimeout)
+    setIsVisible(!isVisible)
+  }, [isVisible, hasTimeout, isSectionVisible])
+
+  const handleOpenSection = useCallback(
+    (selectedSection) => {
+      toggleSection()
+      setSection(selectedSection)
+    },
+    [toggleSection],
+  )
+
+  const handleCloseSection = useCallback(() => {
+    toggleSection()
+    setSection('')
+  }, [toggleSection])
+
+  const handleOutsideClick = useCallback(
+    (event) => {
+      if (wrapperRef && !wrapperRef.contains(event.target) && isSectionVisible) handleCloseSection()
+    },
+    [isSectionVisible, wrapperRef, handleCloseSection],
+  )
+
+  useEffect(() => {
+    openingTimeoutRef = setTimeout(() => {
+      setLoading('')
+    }, 100)
+
+    document.addEventListener('mousedown', handleOutsideClick)
+
+    return () => {
+      if (openingTimeoutRef) clearTimeout(openingTimeoutRef)
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [handleOutsideClick])
+
+  return (
+    <Layout location={location}>
+      <article className={`body ${loading} ${isSectionVisible ? 'is-article-visible' : ''}`}>
+        <section id="wrapper">
+          <Header onOpenSection={handleOpenSection} timeout={hasTimeout} />
+          <Main
+            timeout={hasTimeout}
+            isVisible={isVisible}
+            openSection={article}
+            isSectionVisible={isSectionVisible}
+            onCloseSection={handleCloseSection}
+            setWrapperRef={setWrapperRef}
+          />
+          <Footer timeout={hasTimeout} />
+        </section>
+        <section id="bg" />
+      </article>
+    </Layout>
+  )
+}
+
+HomePage.propTypes = {
+  location: PropTypes.string,
+}
+
+HomePage.defaultProps = {
+  location: '',
+}
+
 export default HomePage
