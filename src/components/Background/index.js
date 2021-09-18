6af43e2c4e2d09bb9144d48444c829f01ee65748
Leonardo Quevedo <leonardoq@trin.ca>
2021-09-18 15:22:37 -0300

    🔥 (v2) Major structure refactor: Initial changes.

---
+++ src/components/Background/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './styles.scss'
+
+const Background = () => <div className="app-background" />
+
+Background.propTypes = {}
+
+export default Background
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -4,18 +4,22 @@ import clsx from 'clsx'
 
 import { useSectionsContext } from 'context/SectionsContext'
 
+import Background from 'components/Background'
+
+import './styles.scss'
+
 const Wrapper = ({ children }) => {
   const { isLoading, currentSection } = useSectionsContext()
 
   return (
     <div
-      className={clsx('body', {
+      className={clsx('app-layout', {
         'is-loading': isLoading,
         'is-article-visible': currentSection && currentSection !== '',
       })}
     >
       <div className="app-wrapper">{children}</div>
-      <div id="site-bg" />
+      <Background />
     </div>
   )
 }
